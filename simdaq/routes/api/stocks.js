a8fc927ea3831b53eddc1a2eae3b14469cd680d9
Yuehan <yuehan.huang@protonmail.com>
2020-12-31 02:13:15 -0800

    Add price api for individual stocks

--- simdaq/routes/api/stocks.js
+++ simdaq/routes/api/stocks.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const { Stock } = require("../../models");
+const { Stock, Price } = require("../../models");
 
-router.get("/", (req, res) => {
+router.get("/index", (req, res) => {
   Stock.findAll()
     .then((stocks) => res.json(stocks))
+    .catch((err) => res.status(400).json(err));
+});
+
+router.get("/prices", (req, res) => {
+  Price.findAll({
+    where: {
+      stockTicker: req.query.stockTicker,
+    },
+    order: [["time", "DESC"]],
+  })
+    .then((prices) => {
+      res.json(prices);
+    })
     .catch((err) => res.status(400).json(err));
 });
 

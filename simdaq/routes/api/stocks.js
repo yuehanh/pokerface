95788711d6e9f710c92082ca941cf4f0297ee26a
Yuehan <yuehan.huang@protonmail.com>
2020-12-31 10:42:11 -0800

    Add  limit on request api

--- simdaq/routes/api/stocks.js
+++ simdaq/routes/api/stocks.js
@@ -6,6 +6,7 @@ const price = require("../../models/price");
 router.get("/index", (req, res) => {
   Stock.findAll()
     .then((stocks) => {
+      //stocks table later may contain company info related to the stock. for now it just has its ticker
       const payload = stocks.map((stock) => ({ ticker: stock.ticker }));
       return res.json(payload);
     })
@@ -30,4 +31,24 @@ router.get("/prices", (req, res) => {
     .catch((err) => res.status(400).json(err));
 });
 
+router.get("/prices/limited", (req, res) => {
+  //impose a max 500 result limit on request
+  const dataLimit = Math.min(req.query.limit || 500, 500);
+  Price.findAll({
+    where: {
+      stockTicker: req.query.stockTicker,
+    },
+    order: [["time", "DESC"]],
+    limit: dataLimit,
+  })
+    .then((prices) => {
+      const payload = prices.map((price) => ({
+        time: price.time,
+        ticker: price.stockTicker,
+        tickerPrice: price.tickerPrice,
+      }));
+      res.json(payload);
+    })
+    .catch((err) => res.status(400).json(err));
+});
 module.exports = router;

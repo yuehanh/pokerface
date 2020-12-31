f352919c9dcaf6e8818dcd76175aefbd14826a16
Yuehan <yuehan.huang@protonmail.com>
2020-12-31 00:29:31 -0800

    Create price migration

---
+++ simdaq/migrations/20201231081916-create-price.js
@@ -0,0 +1,45 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable(
+      "Prices",
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        tickerPrice: {
+          type: Sequelize.DECIMAL,
+        },
+        time: {
+          type: Sequelize.STRING,
+        },
+        stockId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+      },
+      {
+        indexes: [
+          {
+            unique: true,
+            fields: ["time", "stockId"],
+          },
+        ],
+      }
+    );
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("Prices");
+  },
+};

09df143593b5cc97c8568196e2c648fc42f7fd61
Yuehan <yuehan.huang@protonmail.com>
2020-12-31 01:02:05 -0800

    Remove price

--- simdaq/seeders/20201231064224-Stock.js
+++ simdaq/seeders/20201231064224-Stock.js
@@ -6,31 +6,26 @@ module.exports = {
       [
         {
           ticker: "WATER",
-          lastPrice: 4.5,
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
           ticker: "FIRE",
-          lastPrice: 40.12,
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
           ticker: "METAL",
-          lastPrice: 342.11,
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
           ticker: "WOOD",
-          lastPrice: 15.22,
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
           ticker: "DIRT",
-          lastPrice: 0.25,
           createdAt: new Date(),
           updatedAt: new Date(),
         },

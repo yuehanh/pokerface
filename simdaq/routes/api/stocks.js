aeca3ce1d847e9b4b9504af63fc9bbdb6480b16e
Yuehan <yuehan.huang@protonmail.com>
2021-01-04 20:32:51 -0800

    Comment out not used routes

--- simdaq/routes/api/stocks.js
+++ simdaq/routes/api/stocks.js
@@ -12,42 +12,43 @@ router.get("/", (req, res) => {
     .catch((err) => res.status(400).json(err));
 });
 
-router.get("/prices", (req, res) => {
-  Price.findAll({
-    where: {
-      stockTicker: req.query.stockTicker,
-    },
-    order: [["time", "DESC"]],
-  })
-    .then((prices) => {
-      const payload = prices.map((price) => ({
-        time: price.time,
-        ticker: price.stockTicker,
-        tickerPrice: price.tickerPrice,
-      }));
-      res.json(payload);
-    })
-    .catch((err) => res.status(400).json(err));
-});
+// router.get("/prices", (req, res) => {
+//   Price.findAll({
+//     where: {
+//       stockTicker: req.query.stockTicker,
+//     },
+//     order: [["time", "DESC"]],
+//   })
+//     .then((prices) => {
+//       const payload = prices.map((price) => ({
+//         time: price.time,
+//         stockTicker: price.stockTicker,
+//         tickerPrice: price.tickerPrice,
+//       }));
+//       res.json(payload);
+//     })
+//     .catch((err) => res.status(400).json(err));
+// });
 
-router.get("/prices/limited", (req, res) => {
-  //impose a max 500 result limit on request
-  const dataLimit = Math.min(req.query.limit || 500, 500);
-  Price.findAll({
-    where: {
-      stockTicker: req.query.stockTicker,
-    },
-    order: [["time", "DESC"]],
-    limit: dataLimit,
-  })
-    .then((prices) => {
-      const payload = prices.map((price) => ({
-        time: price.time,
-        ticker: price.stockTicker,
-        tickerPrice: price.tickerPrice,
-      }));
-      res.json(payload);
-    })
-    .catch((err) => res.status(400).json(err));
-});
+// router.get("/prices/limited", (req, res) => {
+//   //impose a max 500 result limit on request
+//   const dataLimit = Math.min(req.query.limit || 500, 500);
+//   Price.findAll({
+//     where: {
+//       stockTicker: req.query.stockTicker,
+//     },
+//     order: [["time", "DESC"]],
+//     limit: dataLimit,
+//   })
+//     .then((prices) => {
+//       const payload = prices.map((price) => ({
+//         time: price.time,
+//         stockTicker: price.stockTicker,
+//         tickerPrice: price.tickerPrice,
+//       }));
+//       res.json(payload);
+//     })
+//     .catch((err) => res.status(400).json(err));
+// });
+
 module.exports = router;

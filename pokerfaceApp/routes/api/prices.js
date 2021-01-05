3727bad1175941aa670c092252a88e43728a1b46
Yuehan <yuehan.huang@protonmail.com>
2021-01-05 07:40:43 -0800

    Add websocket broadcast

--- pokerfaceApp/routes/api/prices.js
+++ pokerfaceApp/routes/api/prices.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const wss = require("../../app");
+const WebSocket = require("ws");
 const router = express.Router();
-const { Stock, Price } = require("../../models");
-
+const { Price } = require("../../models");
 
 router.post("/", (req, res) => {
   //can add a token verification method to ensure the data comes from a trusted source
@@ -12,26 +13,17 @@ router.post("/", (req, res) => {
     const tickerPrice = lastPrices[stockTicker];
     try {
       Price.create({ tickerPrice, stockTicker, time });
+      wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify({ lastPrices, time }));
+        }
+      });
     } catch (err) {
+      console.log(err);
       return res.status(400).json(err);
     }
   }
   return res.status(200);
 });
-
-// router.get("/", (req, res) => {
-//   if (req.query.ticker) {
-//     axios
-//       .get(
-//         `http://localhost:5000/api/stocks/prices/limited/?stockTicker=${req.query.ticker}`
-//       )
-//       .then((payload) => {
-//         return res.json(payload.data);
-//       })
-//       .catch((err) => res.status(400).json(err));
-//   } else {
-//     return res.status(400).json("Please specify a Ticker");
-//   }
-// });
 
 module.exports = router;
